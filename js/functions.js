d935a36ed0855c2d5b9a0f608458ea72a54c5343
aaryadalvi2006 <aaryavdalvi@gmail.com>
2025-03-26 16:47:16 +0530

    feature-sec

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
-let parentLink = document.getElementsByClassName("dropdown");console.log(parentLink);
-let childLink = document.getElementsByClassName("nes-links");console.log(childLink);
+// let parentLink = document.getElementsByClassName("dropdown");console.log(parentLink);
+// let childLink = document.getElementsByClassName("nes-links");console.log(childLink);
 
 
-parentLink[0].addEventListener('mouseover', () => {
-   childLink[0].style.display = "block";
-})
+// parentLink[0].addEventListener('mouseover', () => {
+//    childLink[0].style.display = "block";
+// })
 
-parentLink[0].addEventListener('mouseover', () => {
-   childLink[0].style.display = "none";
-})
+// parentLink[0].addEventListener('mouseover', () => {
+//    childLink[0].style.display = "none";
+// })
 
 
 
